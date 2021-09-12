2f2e994e463c4e6bf9e2560e9a7f4a181f0cb471
OSAMA <osama8795145@gmail.com>
2021-09-12 20:15:58 +0500

    olx code

---
+++ src/App.js
@@ -0,0 +1,245 @@
+import React from "react";
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="body">
+
+      <div className="child1">
+
+        <div className="champ">
+
+        <img className="olx" src="https://upload.wikimedia.org/wikipedia/commons/4/42/OLX_New_Logo.png" />
+
+        <select className="cbeta1">
+
+          <option value="karachi">karachi</option>
+          <option value="islamabad">islamabad</option>
+          <option value="panjab">panjab</option>
+          <option value="khaber pakton kha">khaber pakton kha</option>
+          <option value="north karchi">north karachi</option>
+          <option value="nazmabad">nazmabad</option>
+          <option value="karongi">karongi</option>
+          <option value="lahaor">lahaor</option>
+          <option value="sadar">sadar</option>
+          <option value="bagban pura">bagban pura</option>
+          <option value="taj pura">taj pura</option>
+          <option value="woltan">woltan</option>
+          <option value="shadman">shadman</option>
+          <option value="baata pur">baata pur</option>
+          <option value="wabda town">wabda town</option>
+          <option value="karachi">karachi</option>
+          <option value="islamabad">islamabad</option>
+          <option value="panjab">panjab</option>
+          <option value="khaber pakton kha">khaber pakton kha</option>
+          <option value="north karchi">north karachi</option>
+          <option value="nazmabad">nazmabad</option>
+          <option value="karongi">karongi</option>
+          <option value="lahaor">lahaor</option>
+          <option value="sadar">sadar</option>
+          <option value="bagban pura">bagban pura</option>
+          <option value="taj pura">taj pura</option>
+          <option value="woltan">woltan</option>
+          <option value="shadman">shadman</option>
+          <option value="baata pur">baata pur</option>
+          <option value="wabda town">wabda town</option>
+
+        </select>
+        <input className="cbeta2" type="search" placeholder="search" />
+        <a className="cbeta3" href="http://www.Loginonline.com">Login</a>
+        <button className="cbeta4">+ sell</button>
+
+        </div>
+
+      </div>
+
+    <div className="projet">
+
+      <div className="child2">
+
+        <div className="bapb1">
+
+          <select className="betab1" name="ALL CATEGORIES">ALL CATEGORIES
+
+            <option value="ALL CATEGORIES">ALL CATEGORIES</option>
+
+            <option value="Vehicles">Vehicles</option>
+
+            <option value="Mobiles">Mobiles</option>
+
+            <option value="Electronics & Home Appliances">Electronics & Home Appliances</option>
+
+            <option value="Property for sale">Property for sale</option>
+
+            <option value="Animals">Animals</option>
+
+            <option value="Furniture & Home Decor">Furniture & Home Decor</option>
+
+            <option value="Business,Industrial & Agriculture">Business,Industrial & Agriculture</option>
+
+            <option value="Bikes">Bikes</option>
+
+            <option value="Fashion & Beauty">Fashion & Beauty</option>
+
+            <option value="Property for Rent">Property for Rent</option>
+
+            <option value="Jobs">Jobs</option>
+
+            <option value="Services">Services</option>
+
+            <option value="Books, Sports & Hobbies">Books, Sports & Hobbies</option>
+
+            <option value="Kids">Kids</option>
+
+          </select>
+
+        </div>
+
+        <div className="bapb2">
+
+
+          <a className="betab2" href="http://www.Mobile phonesonline.com">Mobile phones</a>
+
+          <a className="betab2" href="http://www.Carsonline.com">Cars</a>
+
+          <a className="betab2" href="http://www.Motorcyclesonline.com">Motorcycles</a>
+
+          <a className="betab2" href="http://www.Housesonline.com">Houses</a>
+
+          <a className="betab2" href="http://www.TV-Video-Audioonline.com">TV-Video-Audio</a>
+
+          <a className="betab2" href="http://www.Tabletsonline.com">Tablets</a>
+
+          <a className="betab2" href="http://www.Land & Poltsonline.com">Land & Polts</a>
+
+        </div>
+
+      </div>
+
+      <div className="child3">
+
+        <img src="http://i.brecorder.com/wp-content/uploads/2017/04/OLX-Pakistan.jpg" height="200px" width="100%" />
+
+
+      </div>
+
+      <div className="child4">
+
+        <h2>Based on your last search </h2>
+
+        <div className="part1">
+
+          <div className="opo">
+            <img src="https://static.toiimg.com/photo/73078527.cms" height="150px" width="150px" />
+
+            <h3>230,000 RS</h3>
+
+          </div>
+
+          <div className="opo">
+
+            <img src="https://mobilemall.pk/public_html/images/no_image.png" height="150px" width="150px" />
+
+            <h3>20,0000 RS</h3>
+
+          </div>
+
+          <div className="opo">
+
+            <img src="https://www.gizbot.com/imgh/310x233/2021/04/xrealme-8-5g_16190771183.jpg.pagespeed.ic.MjxtNNpCTg.jpg" height="150px" width="150px" />
+
+            <h3>99,000 RS</h3>
+
+          </div>
+
+        </div>
+
+        <div className="part1">
+
+          <div className="opo">
+
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5cc3mbZ8HW2tcHBUO79EsqwlrH-i5ShfMiQ&usqp=CAU" height="150px" width="150px" />
+
+            <h3>300,000 RS</h3>
+
+          </div>
+
+          <div className="opo">
+
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW8rtcBX3ZQ6Ok_W78h0zKj94ge9lRJ84E1Q&usqp=CAU" height="150px" width="150px" />
+
+            <h3>250,000 RS</h3>
+
+          </div>
+
+          <div className="opo">
+
+            <img src="https://cache2.pakwheels.com/system/bike_model_pictures/1237/medium/bike_honda_125_cover.jpg?1604303076" height="150px" width="150px" />
+
+            <h3>200,000 RS</h3>
+
+          </div>
+
+        </div>
+
+        <div className="part1">
+
+          <div className="opo">
+
+            <img src="https://cdn.luxe.digital/media/2020/12/16175821/most-expensive-cars-2021-Maserati-MC20-luxe-digital%402x.jpg" height="150px" width="150px" />
+
+            <h3>4500,000 RS</h3>
+
+          </div>
+
+          <div className="opo">
+
+            <img src="https://www.underrinerhonda.com/wp-content/plugins/di-honda-integration/assets/img/hybrid/Honda-Hybrids-Page-2020-CR-V-Hybrid-Touring.png" height="150px" width="150px" />
+
+            <h3>3500,000 RS</h3>
+
+          </div>
+
+          <div className="opo">
+
+            <img src="https://i.pinimg.com/originals/bd/38/15/bd38159ad84ff7155c5e384b2ce58e6b.jpg" height="150px" width="150px" />
+
+            <h3>2400,000 RS</h3>
+
+          </div>
+
+        </div>
+
+     
+
+      </div>
+
+      <div className="child5">
+
+         <a className="betab2" href="http://www.Mobile phonesonline.com">Mobile phones</a>
+
+         <a className="betab2" href="http://www.Carsonline.com">Cars</a>
+
+         <a className="betab2" href="http://www.Motorcyclesonline.com">Motorcycles</a>
+
+         <a className="betab2" href="http://www.Housesonline.com">Houses</a>
+
+         <a className="betab2" href="http://www.TV-Video-Audioonline.com">TV-Video-Audio</a>
+
+         <a className="betab2" href="http://www.Tabletsonline.com">Tablets</a>
+
+         <a className="betab2" href="http://www.Land & Poltsonline.com">Land & Polts</a>
+
+
+
+      </div>
+
+    </div>
+
+
+    </div>
+
+  )
+}
+export default App;
